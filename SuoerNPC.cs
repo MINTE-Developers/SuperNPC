c62448861c1fbe156bbc760576f29aa4c83a8d52
aldinioh <aldin0505@gmail.com>
2025-01-02 13:55:34 +1100

    Add file structure and fix typo

--- SuoerNPC.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SuperNPC
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ SuperNPC.cs
@@ -0,0 +1,88 @@
+﻿using Oxide.Plugins;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Super NPC", "Mint", "1.0.0")]
+    [Description("Make your NPCs real players.")]
+    public class SuperNPC : RustPlugin
+    {
+        #region Fields
+
+
+
+        #endregion
+
+        #region Initalisation
+
+
+
+        #endregion
+
+        #region Helpers
+
+
+
+        #endregion
+
+        #region Commands
+
+
+
+        #endregion
+
+        #region Hooks
+
+
+
+        #endregion
+
+        #region API
+
+
+
+        #endregion
+
+        #region Functions
+
+
+
+        #endregion
+
+        #region Agent
+
+
+
+        #endregion
+
+        #region Core NPC
+
+
+
+        #endregion
+
+        #region Movement
+
+
+
+        #endregion
+
+        #region NPC Sensors
+
+
+
+        #endregion
+
+        #region Localisation
+
+
+
+        #endregion
+
+        #region Configuration
+
+
+
+        #endregion
+    }
+}
